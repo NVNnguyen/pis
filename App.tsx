46414124c7ad10ea3ff6aeeeeebfb5f8c7acfbe8
vietnguyenworld <nguyennvce171903@fpt.edu.vn>
2024-11-19 19:15:56 +0700

    fix app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Lo from "./app/screens/MainPrivateScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 const Stack = createStackNavigator();
-
 const App = () => {
   return <LoginScreen navigation={undefined} />;
 };
 
 export default App;
+
